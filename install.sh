5401a77c073a48eaf55a494561c5249f56d53405
DrunkenFist <146915903+YaBoiFluffie@users.noreply.github.com>
2025-04-28 22:11:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update -y && sudo apt upgrade -y
-sudo apt -y install ruby-dev build-essential git-core postgresql curl nmap gem libsqlite3-dev zlib1g zlib1g-dev libxml2 libxml2-dev libxslt-dev locate libreadline6-dev libcurl4-openssl-dev git-core libssl-dev libyaml-dev openssl autoconf libtool ncurses-dev bison curl wget postgresql postgresql-contrib libpq-dev libapr1 libaprutil1 libsvn1 libpcap-dev
+sudo apt -y install libffi-dev libgmp-dev ruby-dev build-essential git-core postgresql curl nmap gem libsqlite3-dev zlib1g zlib1g-dev libxml2 libxml2-dev libxslt-dev locate libreadline6-dev libcurl4-openssl-dev git-core libssl-dev libyaml-dev openssl autoconf libtool ncurses-dev bison curl wget postgresql postgresql-contrib libpq-dev libapr1 libaprutil1 libsvn1 libpcap-dev
 cd /opt
 git clone --recursive https://github.com/rapid7/metasploit-framework
 cd metasploit-framework
