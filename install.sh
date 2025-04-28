a4bcabfa6c8e1719e222a3851053d502b827154e
DrunkenFist <146915903+YaBoiFluffie@users.noreply.github.com>
2025-04-28 21:21:41 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo apt -y install ruby-dev build-essential git-core postgresql curl nmap gem libsqlite3-dev zlib1g zlib1g-dev libxml2 libxml2-dev libxslt-dev locate libreadline6-dev libcurl4-openssl-dev git-core libssl-dev libyaml-dev openssl autoconf libtool ncurses-dev bison curl wget postgresql postgresql-contrib libpq-dev libapr1 libaprutil1 libsvn1 libpcap-dev
+cd /opt
+git clone --recursive https://github.com/rapid7/metasploit-framework
+cd metasploit-framework
+sudo gem install wirble sqlite3 bundler nokogiri
+sudo gem install bundle
+sudo bundle install
+sudo service postgresql start
+./msfdb init
+./msfconsole
